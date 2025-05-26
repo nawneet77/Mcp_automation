4f73ef5a481d1078313f82ef3f59948bd8c60430
nawneet77 <87024784+nawneet77@users.noreply.github.com>
2025-05-26 12:29:32 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from mcp.server.fastmcp import FastMCP
-
+import webbrowser
 # Create an MCP server
 mcp = FastMCP("NawneetServer")
 
@@ -8,5 +8,14 @@ def greet_nawneet() -> str:
     """Returns a greeting to Nawneet boos"""
     return "Hello Nawneet boos"
 
-if __name__ == "__main__":
-    mcp.run(transport='stdio') 
+@mcp.tool()
+def open_linkedin() -> str:
+    """Opens linkedin in a new browser tab."""
+    webbrowser.open("https://www.linkedin.com/in/nawneet-kumar/")
+    return "Linkedin opened successfully!"
+
+@mcp.tool()
+def open_github() -> str:
+    """Opens github in a new browser tab."""
+    webbrowser.open("https://github.com/nawneet77")
+    return "Github opened successfully!"
